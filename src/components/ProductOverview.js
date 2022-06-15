8fb0b061a2a2085dd4d7857c6248edccd232e75a
Jonathan Värild <Jonathan.varild@gmail.com>
2022-06-16 01:06:57 +0200

    Add product overview page

--- src/components/ProductOverview.js
+++ src/components/ProductOverview.js
@@ -1,16 +1,43 @@
 import React from "react";
-import { faPenToSquare } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBox, faPenToSquare, faBarcode } from "@fortawesome/free-solid-svg-icons";
 
+import apple from "../media/apple.jpg";
+import "../css/ProductOverview.css";
 import Header from "../components/Header";
-import CategorizedList from "../components/CategorizedList";
-import ProductItem from "../components/ProductItem";
+import WidgetBox from "./WidgetBox";
+import SwipableWidgetBox from "./SwipableWidgetBox";
 
 function ProductOverview() {
 	return (
-		<>
+		<div className="productOverview">
 			<Header title="Produktöversikt" icon={faPenToSquare} />
-			
-		</>
+			<WidgetBox className="productName">
+				<FontAwesomeIcon icon={faBox} />
+				<div>
+					<h2>Äpple</h2>
+					<h3>Royal Gala</h3>
+				</div>
+			</WidgetBox>
+			<WidgetBox className="productImage">
+				<img src={apple} alt="" />
+			</WidgetBox>
+
+			<SwipableWidgetBox>
+				<div className="PLUWidget">
+					<FontAwesomeIcon icon={faBarcode} />
+					<div>00 001</div>
+				</div>
+				<div className="PLUWidget">
+					<FontAwesomeIcon icon={faBarcode} />
+					<div>00 002</div>
+				</div>
+				<div className="PLUWidget">
+					<FontAwesomeIcon icon={faBarcode} />
+					<div>00 003</div>
+				</div>
+			</SwipableWidgetBox>
+		</div>
 	);
 }
 
