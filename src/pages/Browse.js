8a3512aace855f2c66e5606f7a37e72dfe4fa0c9
Jonathan Värild <Jonathan.varild@gmail.com>
2022-11-08 19:02:10 +0100

    Removed old debug code

--- src/pages/Browse.js
+++ src/pages/Browse.js
@@ -7,15 +7,12 @@ import ProductItem from "../components/ProductItem";
 
 function Browse() {
 
-	const [info, setInfo] = useState("")
-
 	return (
 		<>
 			<Header title="Produktlista" icon={faFilter} />
 			<CategorizedList title="A">
 				<ProductItem title="Äpple" subtitle="Royal Gala" pluCode="0000" />
 				<ProductItem title="Äpple" subtitle="Royal Gala" pluCode="0000" />
-				<input value={info} onChange={(_, value) => {setInfo(value)}}></input>
 			</CategorizedList>
 			<CategorizedList title="B">
 				<ProductItem title="Äpple" subtitle="Royal Gala" pluCode="0000" />
